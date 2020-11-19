f7354fb862c0da4f660f935e3e3784873fd4767c
sakowskik <sakowskik@users.noreply.github.com>
2020-11-19 11:06:03 -0600

    changes

--- webdev1_portfolio/modal_popout/main.js
+++ webdev1_portfolio/modal_popout/main.js
@@ -1,7 +1,15 @@
-$(".open").on("click", function() {
+!---- $(".open").on("click", function() {
     $(".popup-overlay, .popup-content").addClass("active");
   });
   
   $(".close, .popup-overlay").on("click", function() {
     $(".popup-overlay, .popup-content").removeClass("active");
   });
+  ----!
+
+  $('#box-show').click(function(){
+    $('#box-show').show()
+  })
+  $('#hide').click(function(){
+    $('#box-hide').hide();
+  })
