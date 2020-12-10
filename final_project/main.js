64266e4f13b22cd75b2cc7a5560d400b4d1bf1bc
sakowskik <sakowskik@users.noreply.github.com>
2020-12-10 16:23:20 -0600

    js started

--- final_project/main.js
+++ final_project/main.js
@@ -1,8 +1,9 @@
-let joyImages = ["images/joy1", "images/joy2","images/joy3","images/joy4"];
+let joyImages = ["../images/joy1", "images/joy2","images/joy3","images/joy4",
+];
 
 console.log(joyImages[1])
 
-$('.box1').click(function(){
+$('#box1').click(function(){
 
   let randomIndex = Math.floor(Math.random()*4);
   let imageSrc = joyImages[randomIndex];
